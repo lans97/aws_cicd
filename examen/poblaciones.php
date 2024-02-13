d4f162217485b45d7ad38a1e7f78401d4ff5b38e
Luis Silva <luis.silva1mon@gmail.com>
2024-02-13 07:36:18 -0600

    examen php arrays

---
+++ examen/poblaciones.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="utf-8">
+        <title> Arquitectura Web </title>
+    </head>
+
+    <body>
+
+        <table border="1">
+            <tr>
+                <th>País</th>
+                <th>Población</th>
+            </tr>
+        <?php
+            $paises = array(
+                0 => "México"
+              , 1 => "Alemania"
+              , 2 => "Chile"
+              , 3 => "Emiratos Arabes Unidos"
+              , 4 => "China"
+            );
+
+            $poblacion = array(
+                0 => "120,000,000"
+              , 1 => "50,000,000"
+              , 2 => "30,000,000"
+              , 3 => "70,000,000"
+              , 4 => "1,000,000,000"
+            );
+
+            foreach ($paises as $key => $value) {
+                echo "<tr>";
+                echo "<td>$value</td>";
+                echo "<td>$poblacion($key)</td>";
+                echo "</tr>";
+            }
+        ?>
+        </table>
+    </body>
+</html>
