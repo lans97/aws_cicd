7a17d7abe004b15e4d7f96558069961b80d0e012
Luis Silva <luis.silva1mon@gmail.com>
2024-02-13 07:55:43 -0600

    Indentación y formato

--- examen/poblaciones.php
+++ examen/poblaciones.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html lang="es">
 
-    <head>
-        <meta charset="utf-8">
-        <title> Arquitectura Web </title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title> Arquitectura Web </title>
+</head>
 
-    <body>
-
-        <table border="1">
-            <tr>
-                <th>País</th>
-                <th>Población</th>
-            </tr>
+<body>
+    <table border="1">
+        <tr>
+            <th>País</th>
+            <th>Población</th>
+        </tr>
         <?php
             $paises = array(
                 0 => "México"
-              , 1 => "Alemania"
-              , 2 => "Chile"
-              , 3 => "Emiratos Arabes Unidos"
-              , 4 => "China"
+                , 1 => "Alemania"
+                , 2 => "Chile"
+                , 3 => "Emiratos Arabes Unidos"
+                , 4 => "China"
             );
 
             $poblacion = array(
                 0 => "120,000,000"
-              , 1 => "50,000,000"
-              , 2 => "30,000,000"
-              , 3 => "70,000,000"
-              , 4 => "1,000,000,000"
+                , 1 => "50,000,000"
+                , 2 => "30,000,000"
+                , 3 => "70,000,000"
+                , 4 => "1,000,000,000"
             );
 
             foreach ($paises as $key => $value) {
@@ -37,6 +36,6 @@
                 echo "</tr>";
             }
         ?>
-        </table>
-    </body>
+    </table>
+</body>
 </html>
