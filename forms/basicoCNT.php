33b7ae612c2c7e8844525cf78223c2c134f5f073
Luis Silva <luis.silva1mon@gmail.com>
2024-02-15 08:28:00 -0600

    hello me from the outside

--- forms/basicoCNT.php
+++ forms/basicoCNT.php
@@ -3,6 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST["usuario"];
     $contrasena = $_POSST["contrasena"];
     
+    echo $usuario;
+    echo $contrasena;
+    
     if($usuario == "lanns" && $contrasena == "contra123") {
         echo("Bienvenido");
     }
