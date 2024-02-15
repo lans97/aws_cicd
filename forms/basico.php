45039e63ad67c4fda19015ec751b336b199ebec3
Luis Silva <luis.silva1mon@gmail.com>
2024-02-15 08:51:35 -0600

    combo box

--- forms/basico.php
+++ forms/basico.php
@@ -1,3 +1,24 @@
+<?php
+    $tipos_usuario = array(
+          0 => array (
+              "id_tipo" => 0
+              , "desc" => "Administrador"
+          )
+        , 1 => array (
+              "id_tipo" => 1
+              , "desc" => "Invitado"
+          )
+        , 2 => array (
+              "id_tipo" => 2
+              , "desc" => "Ventas"
+          )
+        , 3 => array (
+              "id_tipo" => 3
+              , "desc" => "Finanzas"
+          )
+    )
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -16,6 +37,14 @@
             <div>
                 <label for="contrasena">Contraseña</label>
                 <input type="password" name="contrasena" id="txt_contrasena">
+            </div>
+            <div>
+                <label for="cmb_tipo_usuario">Tipo de usuario:</label>
+                <select name="tipo_usuario" id="cmb_tipo_usuario">
+                    <?php foreach ($tipos_usuario as $key => $tipo) { ?>
+                        <option value="<?= $tipo["id_tipo"]?>"><?= $tipo["desc"] ?></option>
+                    <?php } ?>
+                </select>
             </div>
             <button type="submit">Ingresar</button>
         </form>
--- forms/basicoCNT.php
+++ forms/basicoCNT.php
@@ -2,9 +2,28 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST["usuario"];
     $contrasena = $_POST["contrasena"];
+    $tipo_usuario = $_POST["tipo_usuario"];
     
     if($usuario == "lanns" && $contrasena == "contra123") {
-        echo("Bienvenido");
+        echo("Bienvenido, ");
+        switch ($tipo_usuario) {
+            case '0':
+                echo "administrador.";
+                break;
+            case '1':
+                echo "invitado.";
+                break;
+            case '2':
+                echo "ventas.";
+                break;
+            case '3':
+                echo "finanzas.";
+                break;
+            
+            default:
+                echo "";
+                break;
+        }
     }
     else {
         header("Location: basico.php");
