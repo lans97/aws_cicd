ef5b4e5940b7bb26adfd8eb78efc02c121b403de
Luis Silva <luis.silva1mon@gmail.com>
2024-04-15 21:19:45 -0600

    schema and fixes

--- controllers/CrudEquiposController.php
+++ controllers/CrudEquiposController.php
@@ -2,13 +2,56 @@
 
 Class CrudEquiposController {
     public function index() {
-        include '../templates/header.php';
-        include '../views/practicas/crud_equipos.php';
-        include '../templates/footer.php';
+        include PROJECT_ROOT . 'templates/header.php';
+        include PROJECT_ROOT . 'views/practicas/crud_equipos.php';
+        include PROJECT_ROOT . 'templates/footer.php';
+    }
+    
+    public function handleSavePC() {
+        $id_equipo = $_POST["id_pc"];
+        $id_marca = $_POST["pc_marca"];
+        $procesador = $_POST["procesador"];
+        $ram = $_POST["ram"];
+        $disco_duro = $_POST["hdd"];
+        $existencia = $_POST["existencia"];
+        
+        $arr_equipos = loadPCs();
+
+        if ($id_equipo == ""){
+            if (filesize("../../files/registroEquipos.csv") == 0) {
+                $id_equipo = 0;
+            } else {
+                $id_equipo = end($arr_equipos)['id_equipo'] + 1; 
+            }
+        }            
+
+        $nueva_pc = array(
+              "id_equipo" => $id_equipo
+            , "id_marca" => $id_marca
+            , "procesador" => $procesador
+            , "ram" => $ram
+            , "disco_duro" => $disco_duro
+            , "existencia" => $existencia
+        );
+
+        $arr_equipos[$id_equipo] = $nueva_pc;
+
+        savePCs($arr_equipos);
+        echo "<script>";
+        echo "alert('Equipo guardado exitosamente');";
+        echo "</script>";
+    }
+
+    public function handleLoadPC() {
+    }
+
+    public function handleDeletePC() {
     }
 }
 
 $crudEquiposController = new CrudEquiposController();
 $crudEquiposController->index();
 
+if (array_key_exists("pc_save", $_POST)) {
+}
 ?>
---
+++ sql/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `Usuario` (
+  `ID_Usuario` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(20) NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `A_Paterno` varchar(20) NOT NULL,
+  `A_Materno` varchar(20) DEFAULT NULL,
+  `Correo` varchar(200) NOT NULL,
+  `Tipo` int(11) NOT NULL,
+  PRIMARY KEY (`ID_Usuario`)
+);
+
+CREATE TABLE IF NOT EXISTS `Marca` (
+    `ID_Marca` int NOT NULL AUTO_INCREMENT,
+    `Nombre` varchar(20) NOT NULL
+    PRIMARY KEY (`ID_Marca`)
+);
+
+CREATE TABLE IF NOT EXISTS `Equipo` (
+    `ID_Equipo` int NOT NULL AUTO_INCREMENT,
+    `ID_Marca` int,
+    `Procesador` varchar(30) NOT NULL,
+    `RAM` varchar(20) NOT NULL,
+    `Disco_Duro` varchar(20) NOT NULL,
+    `Existencia` int not null,
+    PRIMARY KEY (`ID_Equipo`),
+    FOREIGN KEY (`ID_Marca`) REFERENCES `Marca`(`ID_Marca`)
+);
